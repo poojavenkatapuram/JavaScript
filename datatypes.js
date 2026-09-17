01bdc0f322dc144869f43ba945473114e418c7aa
Pooja Venkatapuram <poojavenkatapuram145@gmail.com>
2026-09-17 22:17:24 +0530

    pushing conversionOperations file

--- datatypes.js
+++ datatypes.js
@@ -1,2 +1,21 @@
 "use strict"; //treat all js code as newer version
-alert("hello");
+
+
+let name = "pooja";
+let age = 18;
+let isLoggedIn= false;
+
+console.log(typeof "pooja")
+//Primitive datatypes
+//number =>2 to power 53 is the range of the number in js;
+//bigint => if the number is really big;
+//String => "" or '';
+//boolean => true/false;
+//null =>standalone value;
+// undefined=> this is empty;
+//symbol=> for uniques;
+
+console.log(typeof undefined);//undefined;
+
+console.log(typeof null);//object
+
