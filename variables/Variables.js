1b078d76f43a45947776824036683b93dcaae201
Pooja Venkatapuram <poojavenkatapuram145@gmail.com>
2026-09-17 21:34:37 +0530

    first commit of this repo

---
+++ variables/Variables.js
@@ -0,0 +1,14 @@
+const account_Id = 14555;
+let accountEmail= "pooja@google.com";
+var accountpass = "123";
+accountCity = "jaipur";
+
+// account_Id = 2
+accountEmail = "pa@.com"
+accountpass = "234";
+accountCity = "Hyderabad";
+let accountState;
+
+console.log(account_Id);
+console.table([account_Id, accountEmail, accountpass, accountCity,accountState])
+
