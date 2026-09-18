520b615b231504902a1b770a94d13f2a31a1c828
Pooja Venkatapuram <poojavenkatapuram145@gmail.com>
2026-09-18 10:43:03 +0530

    kadding new file summary_datatypes.js

---
+++ summary_datatypes.js
@@ -0,0 +1,24 @@
+// primitive  
+//non primitive (called by reference)
+
+const score = 100;
+const scoreVal = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId);
+
+const heros = ["conrad", "jeremiah","doga"]
+let myobj={
+    name:"pooja",
+    age:21,
+}
+const myfunction = function(){
+    console.log("hello world");
+}
+console.log(typeof heros)
