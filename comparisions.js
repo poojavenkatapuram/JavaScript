5efb147c495f6d343db72e3dba00e7f72dd62307
Pooja Venkatapuram <poojavenkatapuram145@gmail.com>
2026-09-18 10:41:38 +0530

    updating the folder with new file

---
+++ comparisions.js
@@ -0,0 +1,12 @@
+console.log("2"> 1);
+console.log("02">1); // comparing two different datatypes
+//this is not correct
+
+console.log(null >0);
+console.log(null ==0);
+console.log(null >=0);
+
+console.log(undefined == 0);
+
+//strict check 
+//=== this checks the valuea and also the datatype of that value;
