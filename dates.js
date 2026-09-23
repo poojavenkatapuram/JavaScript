c6b37a444cd518282209f3d0aadf3ae8e098b7ae
Pooja Venkatapuram <poojavenkatapuram145@gmail.com>
2026-09-23 17:40:53 +0530

    added new file dates.js

---
+++ dates.js
@@ -0,0 +1,12 @@
+//Dates
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreatedDAte = new Date(2026,0, 26);
+console.log(myCreatedDAte.toDateString());
+
+
